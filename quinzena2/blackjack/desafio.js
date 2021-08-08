efd73f7203c7533b2127c58ef66fd22ee2ca348f
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2021-08-07 22:55:30 -0300

    projeto blackjack

--- quinzena2/blackjack/desafio.js
+++ quinzena2/blackjack/desafio.js
@@ -1,17 +1,17 @@
 console.log ("Bem vindo(a) ao jogo de Blackjack!")
 if (confirm ("Deseja iniciar uma nova rodada?")) {
-   
+
     const cartas = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"]
     const naipes = ["♦️", "♥️", "♣️", "♠️"]
     let cartaTexto 
     let cartaValor
     let cartaSorteada
-             
+
     function compraCarta () {
         let cartaNumero = cartas [Math.floor(Math.random() * 13)].toString()
         let cartaNaipe = naipes [Math.floor(Math.random() * 4)]
         cartaTexto = (cartaNumero + cartaNaipe)
-                                            
+
             if (cartaNumero === "A"){
                 cartaValor = 11
             } else if (cartaNumero === "K" || cartaNumero === "Q" || cartaNumero === "J") {
@@ -19,15 +19,15 @@ if (confirm ("Deseja iniciar uma nova rodada?")) {
             } else {
                 cartaValor = Number(cartaNumero)
             }
-                     
+
             cartaSorteada = {
                 texto: cartaTexto,
                 valor: cartaValor
             }
-    
+
             return cartaSorteada
     }
-    
+
     let cartaUmUsuario = compraCarta()
     let cartaDoisUsuario = compraCarta()
     let cartaUmComputador = compraCarta ()
@@ -37,13 +37,13 @@ if (confirm ("Deseja iniciar uma nova rodada?")) {
         cartaUmUsuario = compraCarta()
         cartaDoisUsuario = compraCarta()
     }
-    
+
     let pontuacaoUsuario = cartaUmUsuario.valor + cartaDoisUsuario.valor
     let pontuacaoComputador = cartaUmComputador.valor + cartaDoisComputador.valor
-    
+
     console.log (`Usuário - cartas ${cartaUmUsuario.texto} ${cartaDoisUsuario.texto} - pontuação ${pontuacaoUsuario}`)
     console.log (`Computador - cartas ${cartaUmComputador.texto} ${cartaDoisComputador.texto} - pontuação ${pontuacaoComputador}`)
-    
+
     if (pontuacaoUsuario > pontuacaoComputador){
         console.log("O usuário ganhou!")
     } else if (pontuacaoUsuario < pontuacaoComputador){
@@ -51,7 +51,8 @@ if (confirm ("Deseja iniciar uma nova rodada?")) {
     } else {
         console.log("Empate!")
     }
-      
+
  } else {
    console.log ("O jogo acabou")
 }
+
