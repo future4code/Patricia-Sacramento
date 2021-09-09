c046c509fe82424208a147303d80689998b905cf
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2021-09-09 20:58:41 -0300

    componentes funcionais e empregabilidade

---
+++ quinzena5/componentes-funcionais/labedin/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
+          nome="Patrícia" 
+          descricao="Oi, eu sou a Patrícia. Sou professora de História de Educação Básica. Adoro ler e ver séries. Amo meus gatos, Jorge Ricardo e Sebastião Augusto, e minha cachorra, Lola. "
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Governo do Estado de Minas Gerais" 
+          descricao="Professora de História de Educação Básica" 
+        />
+        
+        <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="VIDA" 
+          descricao="Vivendo" 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;
