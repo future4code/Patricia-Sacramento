44e344de14645531c617a7efafbabfa499f8b98b
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2021-09-17 10:14:31 -0300

    alterações no formulário

--- quinzena5/insta4/src/App.js
+++ quinzena5/insta4/src/App.js
@@ -9,36 +9,65 @@ const MainContainer = styled.div`
 
 `
 
+const FormularioNovoPost = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
 class App extends React.Component {
 
   state = {
     postagem: [
       {
+        id:0,
         nomeUsuario: 'paulinha',
         fotoUsuario: 'https://picsum.photos/50/50',
         fotoPost: 'https://picsum.photos/id/1016/200/150',
       },
       {
+        id:1,
         nomeUsuario: 'patricia',
         fotoUsuario: 'https://picsum.photos/id/102/50/50',
         fotoPost: 'https://picsum.photos/id/1000/200/150',
       },
       {
+        id:2,
         nomeUsuario: 'claudia',
         fotoUsuario: 'https://picsum.photos/id/1025/50/50',
         fotoPost: 'https://picsum.photos/id/1003/200/150'
-      },
-      {
-        valorNomeUsuario: '',
-        valorFotoUsuario: '',
-        valorFotoPost: ''
       }
-    ]
+      ],
+           
+      id: '',
+      valorNomeUsuario: '',
+      valorFotoUsuario: '',
+      valorFotoPost: ''      
   }
 
   
+  usuarioNovoPost = (event) => {
+    this.setState({valorNomeUsuario: event.target.value})
+  }
 
+  fotoUsuarioNovoPost = (event) => {
+    this.setState({valorFotoUsuario: event.target.value})
+  }
 
+  fotoNovoPost = (event) =>{
+    this.setState({valorFotoPost: event.target.value})
+  }
+
+  criarNovoPost = () => {
+    this.setState({id: this.state.postagem.length});
+    let novoPost = {
+      id: this.state.id, 
+      nomeUsuario: this.state.valorNomeUsuario,
+      fotoUsuario: this.state.valorFotoUsuario,
+      fotoPost: this.state.valorFotoPost
+    };
+    
+    this.setState(this.state.postagem.push(novoPost))
+  }
 
 
   render() {
@@ -60,11 +89,14 @@ class App extends React.Component {
           fotoPost={this.state.postagem[2].fotoPost}
         />
 
-        
-        <input value={this.state.postagem[3].valorNomeUsuario}>NomeUsuario</input>
-        <input>FotoUsuario</input>
-        <input>FotoPost</input>
-        <button>Enviar</button>
+      
+
+        <FormularioNovoPost>
+          <input value={this.state.valorNomeUsuario} onChange={this.usuarioNovoPost} placeholder="Nome do Usuário"/>
+          <input value={this.state.valorFotoUsuario} onChange={this.fotoUsuarioNovoPost} placeholder="Foto do Usuário"/>
+          <input value={this.state.valorFotoUsuario} onChange={this.fotoNovoPost} placeholder="Foto do Post"/>
+          <button onClick={this.criarNovoPost}>Criar</button>
+        </FormularioNovoPost>
         
       </MainContainer>
     );
