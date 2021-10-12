348229fc602e23492c94091659a3b2bef152a95a
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2021-10-12 10:37:06 -0300

    cria inputs

--- quinzena7/integracao-de-api/integracao-api/src/App.js
+++ quinzena7/integracao-de-api/integracao-api/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TelaCadastro from './components/TelaCadastro';
 
 function App() {
   return (
     <div className="App">
- 
+      <TelaCadastro />
     </div>
   );
 }
