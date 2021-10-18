3f1b631ca72357173f1922d18f35b787676919a7
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2021-10-17 21:38:35 -0300

    cria e deleta playlists

--- quinzena7/Labefy/labefy/src/App.js
+++ quinzena7/Labefy/labefy/src/App.js
@@ -7,10 +7,44 @@ import ListaPlaylists from "./Componentes/ListaPlaylists";
 
 const PaginaContainer = styled.div ` 
   text-align: center;
+  background-color: #FFCCBD;
+  max-height: 100vh;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+
 `
 const LinksPaginas = styled.div` 
   display: flex;
   justify-content: space-evenly;
+  margin-top: 10%;
+
+  >button{
+    border: none;
+    background-color: #FFCCBD;
+    font-size: large;
+    font-weight: 500;
+    border-bottom: 2px solid black;
+
+  }
+`
+const Header = styled.header` 
+  background-color: black;
+  color: #FA6C5F;
+  margin: 0;
+  max-width: 100vw;
+  height: 10%;
+  max-height: 10%;
+
+  >h1{
+    margin: 0;
+    padding: 1%
+  }
+`
+
+const InputNome = styled.div`
+  margin-top: 5%;
+
 `
 class App extends React.Component {
 
@@ -32,18 +66,22 @@ class App extends React.Component {
   }
 
   render (){
-    console.log(this.state.pagina)
     return (
+  
       <PaginaContainer>
-        <header>
+
+        <Header>
           <h1>LABEFY</h1>
-        </header>
+        </Header>
+
         <LinksPaginas>
           <button value="criar-playlist" onClick={this.mudaPagina}>Criar Playlist</button>
           <button value="lista-playlists" onClick={this.mudaPagina}>Lista de Playlists</button>
         </LinksPaginas>
-
-        {this.paginaRenderizada()}
+        
+        <InputNome>
+          {this.paginaRenderizada()}
+        </InputNome>
 
       </PaginaContainer>
     )
--- quinzena7/Labefy/labefy/src/Componentes/ListaPlaylists.js
+++ quinzena7/Labefy/labefy/src/Componentes/ListaPlaylists.js
@@ -1,13 +1,112 @@
 import react from "react";
 import styled from "styled-components";
-import axios from "axios";
+import axios, { Axios } from "axios";
 import React from "react";
 
+const PaginaContainer = styled.div ` 
+  text-align: center;
+  background-color: #FFCCBD;
+  max-height: 100vh;
+  height: 100vh;
+  display: flex;
+  justify-content: center; 
+
+`
+
+const ListadePlaylists = styled.ul` 
+    
+`
+const ItensPlaylists = styled.li` 
+    display: flex;
+    width: 20vw;
+    margin: 3%;
+    font-size: large;
+    align-items: center;
+    
+    >button { 
+        margin-left: 10%;
+        padding: 3%;
+        background-color:#FF5F48 ;
+        outline: none;
+        border: none;
+        font-weight: bold;
+
+        &:active {
+            background-color: #FF907E;
+        }
+    }
+
+`
 
 class ListaPlaylists extends React.Component{
+    state ={
+        playlists: [],
+        playlistsDetalhadas: [],
+    }
+
+    componentDidMount = () => {
+        this.pegaPlaylists()
+    };
+    
+    componentDidUpdate = () => {
+        this.pegaPlaylists()
+    };
+
+    pegaPlaylists = () => {
+        axios.get("https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists", {
+            headers: {
+                Authorization: "patricia-sacramento-banu" 
+            }
+        }).then((resposta) => {
+            this.setState({playlists: resposta.data.result.list})
+        })
+    }
+
+    deletaPlaylist = (event) => {
+        const idPlaylist = event.target.value
+
+        axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${idPlaylist}`, {
+            headers: {
+                Authorization: "patricia-sacramento-banu"
+            }
+        }).then(() => {alert("Playlist removida com sucesso!")
+        }).catch(() => {alert("Playlist não removida. Tente novamente!")})
+
+    }
+
+
+    verDetalhesPlaylist = (event) => {
+        const idPlaylist = event.target.value
+
+        axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${idPlaylist}/tracks`, {
+            headers: {
+                Authorization: "patricia-sacramento-banu"
+            }
+        }).then((resposta) => {
+            this.setState({playlistsDetalhadas: resposta.data.result.tracks})
+        })
+
+    }
+
     render(){
+        const renderPlaylists = this.state.playlists.map((playlist) => {
+            return <ItensPlaylists>{playlist.name}
+            <button value={playlist.id} onClick={this.deletaPlaylist}>Deletar</button>
+            <button value={playlist.id} onClick={this.verDetalhesPlaylist}>Ver mais</button>
+            </ItensPlaylists>
+        }) 
+
+          
+
+
+
         return(
-            <div>Lista de Playlists</div>
+            <PaginaContainer>
+                <ListadePlaylists>
+                    {renderPlaylists}   
+                </ListadePlaylists> 
+
+            </PaginaContainer>
         )
     }
 }
