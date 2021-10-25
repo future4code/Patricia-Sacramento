1c6e47e42edfcb1fadd957e7209e198bfbcd139b
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2021-10-25 19:47:17 -0300

    quase finalizado

--- quinzena8/useeffect/pokemon/src/components/PokeCard/index.js
+++ quinzena8/useeffect/pokemon/src/components/PokeCard/index.js
@@ -1,49 +1,31 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-export function PokeCard () {
-  state = {
-    // valor do estado que guarda infos e foto do pokemon
-    pokemon: {}
-  };
 
-  // método que roda após a montagem do componente
-  componentDidMount() {
-    this.pegaPokemon(this.props.pokemon);
-  }
+export const PokeCard = (props) => {
 
-  // método que roda após a atualização do componente.
-  // Um dos casos de atualização do componente é a
-  // mudança da props que está sendo passado pra ele
-  componentDidUpdate(prevProps) {
-    // aqui, é feita uma verificação da props anterior com a props atual.
-    // Caso a props anterior seja diferente da props atual,
-    // a função pegaPokemon é chamada.
-    if (prevProps.pokemon !== this.props.pokemon) {
-      this.pegaPokemon(this.props.pokemon);
-    }
-  }
+  const [pokemon, setPokemon] = useState({})
 
-  // função que bate na poke API com um nome específico de pokemon
-  // Isso permite que consigamos pegar as infos dos pokemons.
-  // Nos métodos de ciclo de vida, ela é chamada passando como
-  // parâmetro o nome de pokemon que está chegando como props.
-  pegaPokemon = pokeName => {
+  const pegaPokemon = () => {
+
     axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      .get(`https://pokeapi.co/api/v2/pokemon/${props.pokeName}`)
       .then(response => {
-        // guarda as infos do pokemon no estado
-        this.setState({ pokemon: response.data });
+        setPokemon(response.data);
       })
       .catch(err => {
         console.log(err);
       });
   };
 
-
-  const pokemon = this.state.pokemon;
-
+  useEffect (() => {
+    pegaPokemon();
+  
+  }, [props.pokeName]);
+  
+ 
   return (
+
     <div>
       <p>{pokemon.name}</p>
       <p>{pokemon.weight} Kg</p>
@@ -54,6 +36,7 @@ export function PokeCard () {
     </div>
   );
 }
+
 
 
 
