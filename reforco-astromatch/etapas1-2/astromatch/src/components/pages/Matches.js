00eac8b91608558e731764c21375d420c642f766
PatriciaRGSacramento <patriciargsacramento@outlook.com>
2021-10-30 10:10:09 -0300

    lista matches

--- reforco-astromatch/etapas1-2/astromatch/src/components/pages/Matches.js
+++ reforco-astromatch/etapas1-2/astromatch/src/components/pages/Matches.js
@@ -1,4 +1,5 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 
@@ -27,24 +28,36 @@ const MatchContainer = styled.div `
     }
 `
 function Matches (){
+
+    const [matches, setMatches] = useState([])
+
+    const pegaMatches = () => {
+        axios.get("https://us-central1-missao-newton.cloudfunctions.net/astroMatch/patricia/matches")
+        .then((resposta) => {
+            setMatches(resposta.data.matches)
+        })
+        .catch((erro) => {
+            console.log (erro.message)
+        })
+    }
+
+    useEffect(() =>{
+        pegaMatches()
+    }, [])
+
+    const MapMatches = () => {
+       const listaMatches = matches.map((match) => {
+            return <MatchContainer>
+            <img src={match.photo} />
+            <h2>{match.name}</h2>
+        </MatchContainer>
+        })
+        return listaMatches
+    }
     return(
         <PerfilContainer>
-            <MatchContainer>
-                <img src="https://picsum.photos/200" />
-                <h2>Nome da Pessoa</h2>
-            </MatchContainer>
-            <MatchContainer>
-                <img src="https://picsum.photos/200" />
-                <h2>Nome da Pessoa</h2>
-            </MatchContainer>
-            <MatchContainer>
-                <img src="https://picsum.photos/200" />
-                <h2>Nome da Pessoa</h2>
-            </MatchContainer>
-            <MatchContainer>
-                <img src="https://picsum.photos/200" />
-                <h2>Nome da Pessoa</h2>
-            </MatchContainer>
+             {matches.length ? <MapMatches/> : <p> Nada para ver aqui </p>}
+           
         </PerfilContainer>
     )
 }
